3403de2648dd2be650b8557afd4ef7d1136f710b
i319833 <m.mamaev@sap.com>
2018-07-18 17:26:21 +0400

    fix font problem in docs

--- ops/gulp/docs-build.js
+++ ops/gulp/docs-build.js
@@ -5,7 +5,7 @@ let environment = require('../lib/environment');
 
 const task = (cb) => {
 
-    gulpSequence('build:dist', ['docs-resources', 'docs-icons', 'docs-css', 'docs-styleguide'], cb);
+    gulpSequence('build:dist', ['docs-resources', 'docs-icons', 'docs-css', 'docs-styleguide','docs-fonts'], cb);
 
 }
 
