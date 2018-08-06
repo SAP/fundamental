b7e75169e1eb6f81527521071c1c2c020209723b
Hammad Syed <hammad.syed@sap.com>
2018-08-06 09:22:13 -0700

    Revert "Merge branch 'develop-ariba' into develop"
    This reverts commit 172be04101b67a7d2de81955dcfbe5b07bfb25a6, reversing
    changes made to fdb2fadd7a2ea7a8939ce307de62c31695b48748.

--- ops/gulp/docs-build.js
+++ ops/gulp/docs-build.js
@@ -5,7 +5,7 @@ let environment = require('../lib/environment');
 
 const task = (cb) => {
 
-    gulpSequence('build:dist', ['docs-resources', 'docs-icons', 'docs-css', 'docs-theme-ariba', 'docs-styleguide','docs-fonts'], cb);
+    gulpSequence('build:dist', ['docs-resources', 'docs-icons', 'docs-css', 'docs-styleguide','docs-fonts'], cb);
 
 }
 
