e06902c1b3d179733715c746b74e974ed5737074
Ian Quigley <ian.quigley@sap.com>
2018-11-14 16:03:20 +0000

    s

--- docs/ops/gulp/default.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const gulpSequence = require('gulp-sequence');
-
-const task = (cb) => {
-    gulpSequence('docs-build', 'docs-site', 'dev-jekyll', ['dev-serve', 'dev-watch'], cb);
-}
-
-gulp.task('default', task);
-module.exports = task;
--- docs/ops/lib/environment.js
+++
@@ -1,9 +0,0 @@
-const yargs = require('yargs');
-
-var environment = {
-  production: !!(yargs.argv.production),
-  development: !(yargs.argv.production),
-  debug: !!(yargs.argv.debug),
-}
-
-module.exports = environment
