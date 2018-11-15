7ccdf9c72e1f0b6113cce1a8badc8fa7586180ef
Ian Quigley <ian.quigley@sap.com>
2018-11-15 09:32:49 +0000

    Adding support for gulp tasks with netlify

---
+++ docs/gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require("gulp-rename");
+
+const paths = {
+	src: './scss/**/*.scss',
+	dest: './css/'
+}
+
+const task = (cb) => {	
+    gulp.src(paths.src)
+	.pipe(sourcemaps.init())
+	.pipe(sass().on('error', sass.logError))
+	.pipe(rename("fui-site.css"))
+	.pipe(gulp.dest(paths.dest));
+	cb();
+
+}
+
+gulp.task('docs-site', task);
+module.exports = task;
