eaf37991a48e1f1397cc5f861ed909b7f13356ca
Ian Quigley <ian.quigley@sap.com>
2018-11-20 13:16:49 +0000

    updating test execution

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -3,8 +3,10 @@ const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require("gulp-rename");
 const path = require("path");
+const nodemon = require('gulp-nodemon');
+const waitForPort = require('wait-for-port');
 const backstop = require('backstopjs');
-// const test = require('../ops/gulp/test');
+const localAppPort = 3030;
 
 const paths = {
 	src: './scss/**/*.scss',
@@ -23,6 +25,36 @@ const task = (cb) => {
 
 }
 
+// Starts the test server.  If the server port is already in use, it is assumed that the server is already running.
+gulp.task('server:start', function (cb) {	
+    // Check if the server port is already in use, if it is assume the server is started.
+    waitForPort('localhost', localAppPort, {numRetries: 5, retryInterval: 500},
+        function(err) {
+            if (!err) {
+                // port already in use, assume that the server is started.
+                console.log('Server port already in use, assuming that the server is already up');
+                cb();
+            } else {
+                return nodemon({
+                    script: '../test/app.js'
+                }).once('start', function (data) {
+                    waitForPort('localhost', localAppPort, {numRetries: 5, retryInterval: 1000},
+                        function(err) {
+                            if (err) {
+                                throw "Could not connect to port " + localAppPort;
+                            }
+                            cb();
+                        }
+                    );
+                }).on('crash', function() {
+                    process.exit(-1);
+                });
+            }
+        }
+    );
+	
+});
+
 const testVisual = () => {
 	const promise = backstop('test');
 	promise.catch(function (error) {
@@ -38,6 +70,6 @@ const testVisual = () => {
 // require('../ops/gulp/test');
 
 gulp.task('docs-site', task);
-gulp.task('test:visual', testVisual);
+gulp.task('test:visual', ['server:start'], testVisual);
 
 module.exports = task;
