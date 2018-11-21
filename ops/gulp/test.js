eb73dc990d6d630f014fd711004cf5412bb8c4b3
Ian Quigley <ian.quigley@sap.com>
2018-11-21 10:30:29 +0000

    Adding support for gulp 4, removing generated css files.

--- ops/gulp/test.js
+++ ops/gulp/test.js
@@ -34,31 +34,42 @@ gulp.task('server:start', function (cb) {
 	
 });
 
-// Generates a set of reference screenshots from the backstopjs configuration.
-gulp.task('test:reference', [ 'server:start' ], () => { 
+const backstopReference = (cb) => {
     const promise = backstop('reference');
     promise.catch(function (error) {
         // Reference screenshots failed to generate
+        cb(error);
         process.exit(-1);
     });        
     promise.then(function() {
         // Reference screenshots created.
+        cb();
         process.exit(0);
     });
-});
+}
 
-// Tests the current visual elements against the reference screenshots.
-gulp.task('test:visual', [ 'server:start' ], () => {
+const backstopTest = (cb) => {
     const promise = backstop('test');
     promise.catch(function (error) {
         // Tests failed.
+        cb(error);
         process.exit(-1);
     });        
     promise.then(function() {
         // All Tests passed
+        cb();
         process.exit(0);
     });
-});
+};
+
+gulp.task('backstop:test', backstopTest);
+gulp.task('backstop:reference', backstopReference);
+
+// Generates a set of reference screenshots from the backstopjs configuration.
+gulp.task('test:reference', gulp.series('server:start', 'backstop:reference'));
+
+// Tests the current visual elements against the reference screenshots.
+gulp.task('test:visual', gulp.series('server:start', 'backstop:test'));
 
 // Approves any changes to the screenshots and promotes them to the reference data set.
 gulp.task('test:approve', () => backstop('approve'));
