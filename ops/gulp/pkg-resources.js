9720ee0e8dd78f88aa247c21de011e690a5591a5
Saad M <saad.mohiuddin@sap.com>
2018-11-21 09:42:41 -0600

    #977 add image min support

--- ops/gulp/pkg-resources.js
+++ ops/gulp/pkg-resources.js
@@ -1,17 +1,29 @@
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 const config = require('../config');
 const pkg = require('../../package');
 const paths = {
-	src: `${config.root.resources}`,
-	dest: `${config.root.dest}/resources`
+    src: `${config.root.resources}`,
+    dest: `${config.root.dest}/resources`
 }
 
 const task = (cb) => {
-	gulp.src([
-			`${paths.src}/**/*`,
-		])
-		.pipe(gulp.dest(paths.dest));
-	cb();
+    gulp.src([
+        `${paths.src}/**/*`,
+    ])
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+    .pipe(gulp.dest(paths.dest));
+    cb();
 }
 
 gulp.task('pkg-resources', task);
