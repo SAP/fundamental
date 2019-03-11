07105b76faadf2a96e5cb67839a1c6acf1415afe
Greg Smith <greg.smith03@sap.com>
2019-03-11 13:49:12 -0500

    Automate builds and versioning

---
+++ ci-scripts/publish.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -e
+
+git config --global user.email "fundamental@sap.com"
+git config --global user.name "fundamental-bot"
+
+# delete temp branch
+git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" ":$TRAVIS_BRANCH" > /dev/null 2>&1;
+
+std_ver=$(npm run std-version)
+release_tag=$(echo "$std_ver" | grep "tagging release" | awk '{print $4}')
+
+echo "$std_ver"
+
+git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" master > /dev/null 2>&1;
+
+npm run release:create -- --repo $TRAVIS_REPO_SLUG --tag $release_tag --branch master
+
+npm publish
+
+npm run build-doc
+npm run deploy -- --repo "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG"
