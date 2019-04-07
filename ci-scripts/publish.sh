89e6570050bda874dc32aadaab9df76e0b3a81da
Greg Smith <greg.smith03@sap.com>
2019-04-06 22:28:26 -0500

    chore: Release notes (#1376)
    * Moved the create release call below publish to help ensure GitHub is updated
    
    * Updated github-assistant

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -15,12 +15,13 @@ echo "$std_ver"
 
 git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" master > /dev/null 2>&1;
 
-npm run release:create -- --repo $TRAVIS_REPO_SLUG --tag $release_tag --branch master
-
-#build dist folder
+# build dist folder
 npm run build:prod
 
 npm publish
+
+# run this after publish to make sure GitHub finishes updating from the push
+npm run release:create -- --repo $TRAVIS_REPO_SLUG --tag $release_tag --branch master
 
 npm run docs:prod
 npm run deploy -- --repo "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG"
