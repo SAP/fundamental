b814f9687ea30bececc920ce2235b6d2484ef592
jbadan <jenna.badanowski@gmail.com>
2019-04-08 15:28:09 -0700

    updates:

---
+++ config/postcss.config.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+    inline: false,
+    annotation: true,
+    sourcesContent: true,
+    plugins: {
+        autoprefixer: {
+        cascade: true,
+        browsers: ["last 2 versions"]
+    }
+  }
+}
