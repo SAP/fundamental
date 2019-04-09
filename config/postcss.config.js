ec0e49c3c53e12138212ff77b24f18a01c1765f1
jbadan <jenna.badanowski@gmail.com>
2019-04-09 14:42:50 -0700

    dist2 working state

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,13 +1,21 @@
 'use strict'
 
+const packageVersion = require('../package.json').version;
+const year = new Date().getFullYear();
+
 module.exports = {
     inline: false,
     annotation: true,
     sourcesContent: true,
     plugins: {
         autoprefixer: {
-        cascade: true,
-        browsers: ["last 2 versions"]
-    }
+            cascade: true,
+            browsers: ["last 2 versions"]
+        },
+        'postcss-banner': {
+            banner: `Fiori Fundamentals v${packageVersion}
+Copyright (c) ${year} SAP SE or an SAP affiliate company.
+Licensed under Apache License 2.0 (https://github.com/SAP/Fundamental/blob/master/LICENSE)`
+        }
   }
 }
