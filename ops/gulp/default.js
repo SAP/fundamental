f768c49dea80c44cf37c7771e68d843c2434eb24
Jenna Badanowski <29607818+jbadan@users.noreply.github.com>
2019-04-22 08:42:54 -0700

    docs: remove gulp pipeline from docs site build and serve process (#1408)

--- ops/gulp/default.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const FwdRef = require('undertaker-forward-reference');
-
-gulp.registry(FwdRef());
-
-module.exports = gulp.task('default',
-    gulp.series('docs-build', 'docs-site', 'dev-jekyll', gulp.parallel('dev-serve', 'dev-watch')));
