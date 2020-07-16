69d1f5f4d59fdb0e3685910481c500befa2bd44c
deno <mladen.droshev@sap.com>
2020-07-16 18:41:23 -0400

    chore:test release (#1546)
    * chore: test version release
    
    * test: comment in publish-rc script
    
    * chore: update mergable

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -7,7 +7,7 @@ git config --global user.name "fundamental-bot"
 
 npm run std-version -- --prerelease rc --no-verify
 
-git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
+git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH"
 
 #build dist and less folders
 npm run build:prod
