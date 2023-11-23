f7121915b2ac9b83a83c60cc0865edfa00d7783f
kalo2711 <kaloyan.kirilov@sap.com>
2023-11-23 14:58:04 -0500

    fix: final

--- src/app/fl-used/fl-used.component.ts
+++ src/app/fl-used/fl-used.component.ts
@@ -7,9 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlUsedComponent implements OnInit{
   quotes = [
-    { text: 'You may say I am a dreamer, but I am not the only one. I hope someday you will join us. And the world will live as one', author: '~ John Lennon' },
-    { text: 'Your time i slimited, so dont waste it living someone else life. Dont be living with the results of other people thinking', author: '~ Steve Jobs' },
-    { text: 'The greatest glory in living lies not in never falling, but rising everytime we fall', author: '~ Nelson Mandela' }
+    { text: 'Our solution is built on a modern, reactive stack - where micro frontend UI patterns are applied. By splitting monolithic UI into several micro apps, our teams are able to develop and ship things independently with high velocity. This is awesome as each team can deliver customer value fast, but at the same time this confronts us with challenges. Especially when it comes to ensuring a consistent user experience on the interface across the entire web application suite the Fundamental libraries have helped our teams to bring it all together in a nice, consistent way regardless of stack or framework. We use the Fundamental components, examples and documentation nearly every day as part of our design and development lifecycle.', author: '~ SAP FSM' },
+    { text: 'The fundamentals team is amazing to work with! They were willing to work with our existing stack, and continue to go above and beyond to help us meet our consistency goals. They are a true asset and incredibly responsive!', author: '~ SAP Fieldglass' },
   ];
 
   currentQuoteIndex = 0;
