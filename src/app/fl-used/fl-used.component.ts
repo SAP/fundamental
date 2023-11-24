1815800e8a17cd0b95fcc9f05b2e42b7e5372bcd
kalo2711 <kaloyan.kirilov@sap.com>
2023-11-24 13:41:34 -0500

    fix: final touch ups

--- src/app/fl-used/fl-used.component.ts
+++ src/app/fl-used/fl-used.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FlUsedComponent implements OnInit{
   quotes = [
     { text: 'The fundamentals team is amazing to work with! They were willing to work with our existing stack, and continue to go above and beyond to help us meet our consistency goals. They are a true asset and incredibly responsive!', author: '~ SAP Fieldglass' },
-    { text: 'Our solution is built on a modern, reactive stack - where micro frontend UI patterns are applied. By splitting monolithic UI into several micro apps, our teams are able to develop and ship things independently with high velocity. This is awesome as each team can deliver customer value fast, but at the same time this confronts us with challenges. Especially when it comes to ensuring a consistent user experience on the interface across the entire web application suite the Fundamental libraries have helped our teams to bring it all together in a nice, consistent way regardless of stack or framework. We use the Fundamental components, examples and documentation nearly every day as part of our design and development lifecycle.', author: '~ SAP FSM' },
+    { text: 'Our solution employs a modern, reactive stack with micro frontend UI patterns, allowing independent development and rapid deployment. Although this approach enhances team agility, ensuring a unified user experience across the web application suite poses challenges. Fundamental libraries address this by providing consistent design elements, examples, and documentation, streamlining our daily design and development processes.', author: '~ SAP FSM' },
   ];
 
   
